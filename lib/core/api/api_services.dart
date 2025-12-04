9b12045ee65f288df7e0bf968620723191e1af63
yasmin hany <162918255+yoyo3257@users.noreply.github.com>
2025-12-04 05:11:09 +0200

    feat: add api services for market list and search

--- lib/core/api/api_services.dart
+++ lib/core/api/api_services.dart
@@ -1,11 +1,26 @@
-
 import 'package:dio/dio.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
+import '../../features/market/data/models/coin_model.dart';
+import '../../features/market/data/models/search_coin_model.dart';
+import '../../features/market/data/models/search_response.dart';
+import 'end_points.dart';
 
 part 'api_services.g.dart';
 
-@RestApi(baseUrl: "Constants.baseUrl")
+@RestApi(baseUrl: EndPoints.baseUrl)
 abstract class ApiService {
-  factory ApiService(Dio dio) = _ApiService;
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET(EndPoints.markets)
+  Future<List<CoinModel>> getMarketCoins(
+    @Query("vs_currency") String currency,
+    @Query("order") String order,
+    @Query("per_page") int perPage,
+    @Query("page") int page,
+  );
+  @GET(EndPoints.search)
+  Future<SearchResponse> searchCoins(
+    @Query("query") String keyword,
+  );
 }
