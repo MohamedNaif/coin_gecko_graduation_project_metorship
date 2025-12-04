f837440524b036e7bcea2ff88343f5ce6eb0c058
yasmin hany <162918255+yoyo3257@users.noreply.github.com>
2025-12-04 05:12:29 +0200

    feat: add market screen route

--- lib/config/routing/app_router.dart
+++ lib/config/routing/app_router.dart
@@ -1,13 +1,14 @@
-
 import 'package:coin_gecko_graduation_project_metorship/config/routing/routes.dart';
 import 'package:flutter/material.dart';
+
+import '../../features/market/presentation/pages/market_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case Routes.splash:
-        return MaterialPageRoute(builder: (_) => const Scaffold());
-     
+      case Routes.market:
+        return MaterialPageRoute(builder: (_) => const MarketScreen());
+
       default:
         return null;
     }
