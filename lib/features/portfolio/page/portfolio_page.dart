828f09db211ba34b715b6961f1690b38922c0bf6
Abdelrahman <Aa5282243@gmail.com>
2025-12-10 17:50:45 +0200

    feat: Implement screen protection on the portfolio page and update project dependencies.

--- lib/features/portfolio/page/portfolio_page.dart
+++ lib/features/portfolio/page/portfolio_page.dart
@@ -1,8 +1,36 @@
 import 'package:coin_gecko_graduation_project_metorship/features/portfolio/widgets/portfolio_body.dart';
 import 'package:flutter/material.dart';
+import 'package:screen_protector/screen_protector.dart';
 
-class PortfolioPage extends StatelessWidget {
+class PortfolioPage extends StatefulWidget {
   const PortfolioPage({super.key});
+
+  @override
+  State<PortfolioPage> createState() => _PortfolioPageState();
+}
+
+class _PortfolioPageState extends State<PortfolioPage> {
+  void _preventScreenshotOn() async =>
+      await ScreenProtector.preventScreenshotOn();
+
+  void _preventScreenshotOff() async =>
+      await ScreenProtector.preventScreenshotOff();
+
+  void _protectDataLeakageWithBlur() async =>
+      await ScreenProtector.protectDataLeakageWithBlur();
+
+  @override
+  void initState() {
+    _preventScreenshotOn();
+    _protectDataLeakageWithBlur();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _preventScreenshotOff();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
